820ca8fc3a6696d72c411b968a3c9bbd99bce57f
ajogerio <aaogerio@up.edu.ph>
2025-05-07 00:12:29 +0800

    chore: setup scripts and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -10,4 +10,14 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", 
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Disable the rule that requires React to be in scope
+    },
+  },
 ]);
